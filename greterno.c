903cbedd1a55f4797ee8f6cb0448c6c86282d36e
Pritee Tajanpure <122800124+Priteetajanpure30@users.noreply.github.com>
2023-01-30 15:53:05 +0530

    Add files via upload

---
+++ greterno.c
@@ -0,0 +1,21 @@
+//program for greater no.
+#include<iostream>
+using namespace std;
+main()
+{
+	int a,b;
+	cout<<"Enter 2 no.";
+	cin>>a>>b;
+	if(a>b)
+	{
+		cout<<"A is greater";
+	}
+	else if(b>a)
+	{
+		cout<<"a is greater";
+	}
+	else
+	{
+		cout<<"equal";
+	}
+}
