41bdae56418dece243a663719cac8b0838d79bf1
Pritee Tajanpure <122800124+Priteetajanpure30@users.noreply.github.com>
2023-01-30 15:51:02 +0530

    Add files via upload

---
+++ evean odd.cpp
@@ -0,0 +1,16 @@
+//program for even ane odd no
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter a number:");
+	scanf("%d", &n);
+	if(n%2>=0)
+	{
+		printf("Number is even");
+	}
+	else
+	{
+		printf("Number is odd");
+	}
+}
