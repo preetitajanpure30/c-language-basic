3395c60b98b41923c9311e5a88845483b03855c8
Pritee Tajanpure <122800124+Priteetajanpure30@users.noreply.github.com>
2023-02-02 13:19:53 +0530

    Create inheritance.c

---
+++ inheritance.c
@@ -0,0 +1,25 @@
+//program for single inheritance
+#include<iostream>
+using namespace std;
+class A
+{
+	public:
+		int a;
+		void display()
+		{
+			cout<<"\n A";
+		}
+};
+class b; publicA
+{
+	public:
+		int b;
+		void show()
+		{
+			cout<<"\n B";
+		}
+};
+main()
+B b;
+B1.display();
+B1.show();
